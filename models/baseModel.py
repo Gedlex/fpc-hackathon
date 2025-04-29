00b278253e0a745af7c2e16b70e5a00b8424f022
Luca Vignola <lucav@MacBook-Air.local>
2025-04-29 11:22:46 +0200

    working animation with static asteroid

--- models/baseModel.py
+++ models/baseModel.py
@@ -21,6 +21,25 @@ class BaseModel:
     def animateSimulation(self, x_trajectory: np.ndarray, u_trajectory: np.ndarray):
         raise NotImplementedError("Subclasses must implement animateSimulation.")
 
+    def define_constraint(x: np.ndarray, asteroid: np.ndarray):
+        x_asteroid = asteroid[0]
+        z_asteroid = asteroid[1]
+        x_rocket = x[0]
+        z_rocket = x[1]
+        radius_asteroid = asteroid[2]
+        radius_rocket = 1
+        # Define the constraint function
+        dist_squared = (x_rocket - x_asteroid)**2 + (z_rocket - z_asteroid)**2
+        min_dist_squared = (radius_asteroid + radius_rocket)**2
+        return min_dist_squared - dist_squared <= 0
+
+
+
+    def createAsteroid(self, x: np.ndarray):
+        x_asteroid = x[0]
+        z_asteroid = x[1] + 2
+        radius_asteroid = 0.5
+        return np.array([x_asteroid, z_asteroid, radius_asteroid])
 
     def simulateClosedLoop(self, sim_length:int, x_init:np.ndarray, controller: Callable, num_agents:int=1):
         nx = self.model_config.nx
@@ -28,13 +47,15 @@ class BaseModel:
         x_trajectory = np.zeros((nx * num_agents, sim_length+1))
         u_trajectory = np.zeros((nu * num_agents, sim_length))
         x_trajectory[:, 0] = x_init.flatten()
+        asteroid_coords = self.createAsteroid(x_init)
         for i in range(sim_length):
+            
             u = controller(x_trajectory[:, i])
             u_trajectory[:, i] = u
             for i_agent in range(num_agents):
                 x_trajectory[i_agent*nx:(i_agent+1)*nx, i+1] = self.f_disc(x_trajectory[i_agent*nx:(i_agent+1)*nx, i], u[i_agent*nu:(i_agent+1)*nu]).full().flatten()
             print(f"Step {i+1}: x = {x_trajectory[:, i+1]}, u = {u}")
-        return x_trajectory, u_trajectory
+        return x_trajectory, u_trajectory, asteroid_coords
 
 
     def simulateOpenLoop(self, x_init: np.ndarray, u_trajectory: np.ndarray, num_agents:int=1):
